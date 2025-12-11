0e1b0b91591e99c4aa52977a0467e0f44a2b2e26
Ankit Bhuwad <124443636+Ankit73-bit@users.noreply.github.com>
2025-12-11 16:23:41 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# EXPENSE TRACKER
+
+## Display current Month
+
+### Goal
+
+- Default current month
+- user can select privious month
+  - base on selection show expense of that month
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ExpenseTracker from "./fetures/expneseTracker/ExpenseTracker";
+
+function App() {
+  return (
+    <>
+      <ExpenseTracker />
+    </>
+  );
+}
+
+export default App;
